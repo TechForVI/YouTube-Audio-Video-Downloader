f2eff50822aae20dd697d8a8f54904d44f69b546
TechForVI <sabirrana112211@gmail.com>
2026-01-17 18:35:36 +0500

    Update get_code.js

--- api/get_code.js
+++ api/get_code.js
@@ -1,1 +1,18 @@
-gb
+const axios = require('axios');
+
+export default async function handler(req, res) {
+  // آپ کی فائل کا RAW لنک
+  const github_raw_url = "https://raw.githubusercontent.com/TechForVI/YouTube-Audio-Video-Downloader/main/youtube.lua";
+
+  try {
+    const response = await axios.get(github_raw_url);
+    
+    // سیکیورٹی کے لیے Content-Type سیٹ کریں
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.setHeader('Cache-Control', 'no-store'); // تاکہ کوڈ پرانا لوڈ نہ ہو
+    
+    res.status(200).send(response.data);
+  } catch (error) {
+    res.status(500).send("-- Error: Could not fetch code from GitHub");
+  }
+}
